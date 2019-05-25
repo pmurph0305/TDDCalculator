dc6b5c3785386c7d1f5f3fcfb8e4a8c957b8a0bc
pmurph0305 <pmurph0305@gmail.com>
2019-05-25 10:21:33 -0400

    Display css

--- src/components/Display/Display.jsx
+++ src/components/Display/Display.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+import './Display.css';
+
 const Display = ({ displayedValue }) => (
   <div className="display-container">
     <p className="display-value">{displayedValue}</p>
